588e2f495f05712a5bc12bbf1d59b80d1057a1f6
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-14 18:21:12 -0500

    composite pattern test added

--- src/test/java/com/designpatterns/app/structural/composite/CompositePatternTest.java
+++ src/test/java/com/designpatterns/app/structural/composite/CompositePatternTest.java
@@ -2,9 +2,18 @@ package com.designpatterns.app.structural.composite;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CompositePatternTest {
     @Test
     public void testCompositePattern() {
+        Airforce natoAlliance = new Airforce();
+        F16 frenchF16 = new F16();
+        C130Hercules germanCargoPlane = new C130Hercules();
 
+        natoAlliance.add(frenchF16);
+        natoAlliance.add(germanCargoPlane);
+
+        assertEquals(7, natoAlliance.getPersonnel());
     }
 }
