a3342e54658a04ecafabb3f093b82f85b992fecd
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-14 19:55:02 -0500

    Decorator pattern implemented

---
+++ src/main/java/com/designpatterns/app/structural/decorator/BoeingDecorator.java
@@ -0,0 +1,6 @@
+package com.designpatterns.app.structural.decorator;
+
+//requirement of the decorator pattern - start with an abstract decorator
+public abstract class BoeingDecorator implements IAircraft {
+
+}
---
+++ src/main/java/com/designpatterns/app/structural/decorator/BulletProof.java
@@ -0,0 +1,24 @@
+package com.designpatterns.app.structural.decorator;
+
+public class BulletProof extends BoeingDecorator{
+    IAircraft boeing;
+
+    public BulletProof(IAircraft boeing) {
+        this.boeing = boeing;
+    }
+
+    @Override
+    public void fly() {
+        boeing.fly();
+    }
+
+    @Override
+    public void land() {
+        boeing.land();
+    }
+
+    @Override
+    public float getWeight() {
+        return 50f + boeing.getWeight();
+    }
+}
