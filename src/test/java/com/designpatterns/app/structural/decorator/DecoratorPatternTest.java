0d1ad02b462a34510cfc99fea03761d0719c0e0a
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-14 19:24:07 -0500

    Decorator pattern initial commit

---
+++ src/test/java/com/designpatterns/app/structural/decorator/DecoratorPatternTest.java
@@ -0,0 +1,10 @@
+package com.designpatterns.app.structural.decorator;
+
+import org.junit.jupiter.api.Test;
+
+public class DecoratorPatternTest {
+    @Test
+    public void decoratorPatternTest() {
+
+    }
+}
