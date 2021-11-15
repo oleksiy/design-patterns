145a935190af3bb442234424d975df21cbba138d
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-14 20:28:41 -0500

    flyweight pattern

---
+++ src/test/java/com/designpatterns/app/structural/flyweight/FlyweightPatternTest.java
@@ -0,0 +1,10 @@
+package com.designpatterns.app.structural.flyweight;
+
+import org.junit.jupiter.api.Test;
+
+public class FlyweightPatternTest {
+    @Test
+    public void testFlyweightPatternStructure() {
+
+    }
+}
