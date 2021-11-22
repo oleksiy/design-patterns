ee0620b7987ae063f1ee7b616fb39cd84da029d4
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-21 22:33:29 -0500

    added test and implementation of the memento pattern

--- src/main/java/com/designpatterns/app/behavioral/memento/Something.java
+++
@@ -1,4 +0,0 @@
-package com.designpatterns.app.behavioral.memento;
-
-public class Something {
-}
--- src/test/java/com/designpatterns/app/behavioral/memento/MementoPatternTest.java
+++ src/test/java/com/designpatterns/app/behavioral/memento/MementoPatternTest.java
@@ -1,4 +1,21 @@
 package com.designpatterns.app.behavioral.memento;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MementoPatternTest {
+    @Test
+    public void testMementoBehavior() throws IOException, ClassNotFoundException {
+        BlackBox blackBox = new BlackBox(12350L, 88.2, 225.08f);
+        byte[] memento = blackBox.getState();
+        assertEquals(88.2, blackBox.getSpeed());
+        blackBox.setSpeed(0);
+        assertEquals(0, blackBox.getSpeed());
+        //restore state
+        blackBox = blackBox.setState(memento);
+        assertEquals(88.2, blackBox.getSpeed());
+    }
 }
