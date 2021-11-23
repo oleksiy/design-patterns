8c313e642c02a5e203d7107bd11127b02b2455e3
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-22 19:36:32 -0500

    State machine added

---
+++ src/main/java/com/designpatterns/app/behavioral/state/F16.java
@@ -0,0 +1,65 @@
+package com.designpatterns.app.behavioral.state;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class F16 implements IAircraft {
+
+    private ParkedState parkedState = new ParkedState(this);
+    private CrashState crashState = new CrashState(this);
+    private LandState landState = new LandState(this);
+    private TaxiState taxiState = new TaxiState(this);
+    private AirborneState airborneState = new AirborneState(this);
+    IPilotActions state;
+
+    @Override
+    public void fly() {
+        log.info("Flying F16");
+    }
+
+    @Override
+    public void land() {
+        log.info("Landing F16");
+    }
+
+    void startsEngine() {
+        state.pilotTaxies(this);
+    }
+
+    void fliesPlane() {
+        state.pilotFlies(this);
+    }
+
+    void landsPlane() {
+        state.pilotEjects(this);
+    }
+
+    void setState(IPilotActions IPilotActions) {
+        state = IPilotActions;
+    }
+
+    ParkedState getParkedState() {
+        return parkedState;
+    }
+
+    ParkedState getParkState() {
+        return parkedState;
+    }
+
+    CrashState getCrashState() {
+        return crashState;
+    }
+
+    LandState getLandState() {
+        return landState;
+    }
+
+    TaxiState getTaxiState() {
+        return taxiState;
+    }
+
+    public AirborneState getAirborneState() {
+        return airborneState;
+    }
+
+}
---
+++ src/main/java/com/designpatterns/app/behavioral/state/IAircraft.java
@@ -0,0 +1,9 @@
+package com.designpatterns.app.behavioral.state;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+public interface IAircraft {
+    public void fly();
+    public void land();
+}
