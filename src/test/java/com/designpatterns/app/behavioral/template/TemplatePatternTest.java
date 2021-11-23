26905d830a45e2f36ef3bd3931f736489e68fb01
Oleksiy Blavat <blavat.alex@gmail.com>
2021-11-22 20:19:33 -0500

    adding template example

---
+++ src/test/java/com/designpatterns/app/behavioral/template/TemplatePatternTest.java
@@ -0,0 +1,10 @@
+package com.designpatterns.app.behavioral.template;
+
+import org.junit.jupiter.api.Test;
+
+public class TemplatePatternTest {
+    @Test
+    public void testStructurePattern() {
+
+    }
+}
